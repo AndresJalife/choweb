71d24f5529258015d0c2724cd33d67c9208d3884
Andres Jalife <ajalife@fi.uba.ar>
2021-04-18 18:16:11 -0300

    página principal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+import {
+  Switch,
+  Route,
+  useHistory
+} from "react-router-dom";
+import Main from './Pages/Main/MainPage';
+import logo from './resources/img/chowLogo.jpeg';
+import Contacto from './Pages/Contacto/ContactoPage';
+import Portfolio from './Pages/Portfolio/PortfolioPage';
+import Nosotros from './Pages/Nosotros/NosotrosPage';
 
 function App() {
+  const history = useHistory();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div id="rightHeader" className="headerButtonsContainer">
+              <div className="headerButton" onClick={() => history.push('/portfolio')}>
+                  <p> Portfolio</p>
+              </div>
+              
+              <div className="headerButton">
+                <p>Nosotros </p>
+              </div>
+              <div className="headerButton">
+                <p> Contacto</p>
+              </div>
+          </div>
+          <div className="imageContainer">
+            <img src={logo} id="imageLogo" onClick={() => history.push('/')}/>
+          </div>
+          <div id="leftHeader" className="headerButtonsContainer">
+            {/* <img src="https://img.icons8.com/fluent-systems-regular/36/ffffff/facebook-new--v1.png"/> */}
+              {/* <img src="https://img.icons8.com/material/40/ffffff/instagram-new--v1.png"/>
+              <img src="https://img.icons8.com/ios-glyphs/50/ffffff/youtube-play.png"/> */}
+
+          </div>
+
       </header>
+      <main>
+        <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contacto" component={Contacto} />
+            <Route exact path="/nosotros" component={Nosotros} />
+
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/Pages/Main/MainPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Main.css'
+import mainVideoMP4 from '../../resources/video/mainVideo/movie.mp4';
+import mainVideoWEB from '../../resources/video/mainVideo/movie.webm';
+import Video from "../../Components/Video";
+
+export default class MainPage extends Component {
+    render() {
+        return (
+            <div>
+                <Video class="video" mp4={mainVideoMP4} webm={mainVideoWEB} />
+            </div>
+        )
+    }
+}
